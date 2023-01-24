d4e363529957dc61147000073e63f4c354c52aa5
thephez <thephez@users.noreply.github.com>
2023-01-24 16:35:06 -0500

    build: enable hoverxref for all refs

--- conf.py
+++ conf.py
@@ -90,6 +90,8 @@ extensions = [
     'hoverxref.extension'
 ]
 
+hoverxref_auto_ref = True
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
